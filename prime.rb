38e5246ec5b5cb2e189f34ac26d51d17a6eadcf3
Ben Dunnell <bjdunnell96@gmail.com>
2019-09-19 13:31:58 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,46 +1,15 @@
-# def prime?(n)
 
-#   if n <= 1
-#         return false
-#   end
-
-#   i = 2
-#   # return n = Math.sqrt(n).floor
-#     while i < n
-#       if(n % i == 0)
-
-#       return false
-#       i += 1
-#     end
-
-#       return true
-#     end
-# end
-
-
-# function isPrime(num){
-#   for(var i = 2; i < num; i++){
-#      if(num % i === 0){
-#            return false;
-#      }
-#   }
-#   return true;
-# }
-
-def prime?(n)
-  if n <= 1
-    return false
-  elsif  n <= 3
-    return true
-  elsif  n % 2 == 0 || n % 3 == 0
+def prime?(num)
+  if num == 0 || num == 1
     return false
   end
-
-  for counter in 2..Math.sqrt(n) 
-    if n % counter == 0
+  test_factor = num - 1
+  while test_factor > 1
+    quotient = num.to_f / test_factor
+    if quotient == quotient.floor
       return false
-      counter += 1
+    end
+    test_factor -= 1
   end
-  return true
-end
+  true
 end 
